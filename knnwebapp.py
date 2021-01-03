eb7ea15c91e1bb520f20e5f0cf50a7f650956562
raghavendrakulkarni14 <71723421+raghavendrakulkarni14@users.noreply.github.com>
2021-01-03 21:49:05 +0530

    Create knnwebapp.py

---
+++ knnwebapp.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pickle
+
+
+
+knn_model=pickle.load(open('KNNModel.pkl','rb'))
+
+def classify(num):
+def main():
+    st.title("Streamlit Tutorial")
+    html_temp = """
+    <div style="background-color:teal ;padding:10px">
+    <h2 style="color:white;text-align:center;">Iris Classification</h2>
+    </div>
+    """
+    st.markdown(html_temp, unsafe_allow_html=True)
+   
+    st.subheader(option)
+    sl=st.slider('Select Sepal Length', 0.0, 10.0)
+    sw=st.slider('Select Sepal Width', 0.0, 10.0)
+    pl=st.slider('Select Petal Length', 0.0, 10.0)
+    pw=st.slider('Select Petal Width', 0.0, 10.0)
+    inputs=[[sl,sw,pl,pw]]
+    if st.button('Predict'):
+        st.success(classify(lin_model.predict(inputs)))
+        
+
+
+if __name__=='__main__':
+    main()
